d2539332641d874ddd6a0053ecb91399e9012219
Nordine Bittich <contact@bittich.be>
2018-10-13 13:19:04 +0200

    dynamic url api

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN npm install -g @angular/cli@6.1.2
 COPY package*.json ./
 RUN npm install
 COPY . .
+RUN sed -i "s/REST_API_URL/$REST_API_URL/g" src/app/data.service.ts
 RUN ng build
 EXPOSE 4200
 CMD [ "ng", "serve", "--host","0.0.0.0" ]
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,30 +10,30 @@ export class DataService {
     constructor(private http:HttpClient){}
 
     postInvoice(invoice:Invoice,callback?){
-        this.http.post("http://127.0.0.1:8080/invoice",invoice).subscribe(()=>callback());
+        this.http.post("REST_API_URL/invoice",invoice).subscribe(()=>callback());
     }
 
     getInvoices(){
-        return this.http.get<Invoice[]>("http://127.0.0.1:8080/invoice");
+        return this.http.get<Invoice[]>("REST_API_URL/invoice");
     }
 
     postInvoiceProfile(invoiceProfile:InvoiceProfile,callback?){
-        this.http.post("http://127.0.0.1:8080/invoiceprofile", invoiceProfile ).subscribe(()=>callback());
+        this.http.post("REST_API_URL/invoiceprofile", invoiceProfile ).subscribe(()=>callback());
     }
 
     getInvoiceProfiles(){
-        return this.http.get<InvoiceProfile[]>("http://127.0.0.1:8080/invoiceprofile");
+        return this.http.get<InvoiceProfile[]>("REST_API_URL/invoiceprofile");
     }
 
     deleteInvoiceProfile(invoiceProfile:InvoiceProfile,callback?){
-        this.http.delete("http://127.0.0.1:8080/invoiceprofile",{params:{id:invoiceProfile.id}}).subscribe(()=>callback());
+        this.http.delete("REST_API_URL/invoiceprofile",{params:{id:invoiceProfile.id}}).subscribe(()=>callback());
     }
 
     deleteInvoice(invoice:Invoice, callback?){
-        this.http.delete("http://127.0.0.1:8080/invoice",{params:{id:invoice.id}}).subscribe(()=>callback());
+        this.http.delete("REST_API_URL/invoice",{params:{id:invoice.id}}).subscribe(()=>callback());
     }
     generatePdf(invoice: Invoice): any {
-        this.http.get("http://127.0.0.1:8080/invoice/generatepdf",{
+        this.http.get("REST_API_URL/invoice/generatepdf",{
             params:{id:invoice.id},
             responseType:"arraybuffer"
         }).subscribe(
