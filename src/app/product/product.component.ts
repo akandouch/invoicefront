16d1fd11e78cb7dea172a4a0d17367b3c0e3b9ce
Nordine Bittich <contact@bittich.be>
2018-11-02 13:37:27 +0100

    translate invoice product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, EventEmitter, Output, Input, Inject } from '@angular/core';
-import { Product, ProductType, UnitOfMeasure } from './product.class';
-import { faPlus,faEllipsisH,faEye,faEdit,faCopy,faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { RestService } from '../services/restservice.interface';
-import { ProductRestServiceImpl } from '../services/productrestserviceimpl.class';
-import { DataColumn } from '../data-table/data-table.component';
-import { Entity } from '../entity.interface';
+import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
+import {Product, ProductType, UnitOfMeasure} from './product.class';
+import {faCopy, faEdit, faEllipsisH, faEye, faPlus, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {RestService} from '../services/restservice.interface';
+import {ProductRestServiceImpl} from '../services/productrestserviceimpl.class';
+import {DataColumn} from '../data-table/data-table.component';
 
 @Component({
   selector: 'app-product',
@@ -13,33 +12,38 @@ import { Entity } from '../entity.interface';
 })
 export class ProductComponent implements OnInit {
 
-  public products:Product[];
-  public _filteredProducts:Product[];
+  public products: Product[];
+  public _filteredProducts: Product[];
 
-  faPlus = faPlus; faEllipsisH = faEllipsisH;faEye = faEye;faEdit = faEdit;faCopy = faCopy;faTrashAlt = faTrashAlt;
+  faPlus = faPlus;
+  faEllipsisH = faEllipsisH;
+  faEye = faEye;
+  faEdit = faEdit;
+  faCopy = faCopy;
+  faTrashAlt = faTrashAlt;
 
   @Output()
   productSelected = new EventEmitter<Product>();
 
   @Input()
-  selectOnly:boolean = false;
+  selectOnly: boolean = false;
 
-  typeFilter:number;
-  nameFilter:string;
+  typeFilter: number;
+  nameFilter: string;
 
   /* for data table */
-  public dataColumns:Array<DataColumn> = [
-    {field:{name:"id"},label:"Id"},
-    {field:{name:"name"}, label:"Name"},
-    {field:{name:"description"}, label:"Description"},
-    {field:{name:"quantity"}, label:"Quantity"},
-    {field:{name:"unitOfMeasure"}, label:"Unif of measure"},
-    {field:{name:"unitPrice"}, label:"Unit price"},
-    {field:{name:"vat"}, label: "Vat"},
-    {field:{name:"type"}, label:"Type"}
+  public dataColumns: Array<DataColumn> = [
+    {field: {name: 'id'}, label: 'page.product.id'},
+    {field: {name: 'name'}, label: 'common.name'},
+    {field: {name: 'description'}, label: 'common.description'},
+    {field: {name: 'quantity'}, label: 'page.items.qty'},
+    {field: {name: 'unitOfMeasure'}, label: 'page.items.unitOfMeasure'},
+    {field: {name: 'unitPrice'}, label: 'page.items.priceUnit'},
+    {field: {name: 'vat'}, label: 'page.items.vatRate'},
+    {field: {name: 'type'}, label: 'common.type'}
   ];
-  
-  constructor(@Inject(ProductRestServiceImpl) private productRestService:RestService) { 
+
+  constructor(@Inject(ProductRestServiceImpl) private productRestService: RestService) {
     this.products = new Array();
     this.reload();
   }
@@ -47,67 +51,71 @@ export class ProductComponent implements OnInit {
   ngOnInit() {
   }
 
-  select(product:Product){
-    this.productSelected.emit(Object.assign(new Product,product));
+  select(product: Product) {
+    this.productSelected.emit(Object.assign(new Product, product));
   }
 
-  get filteredProduct(){
-    console.log('test')
-    var filteredList= this.products;
-    if(this.typeFilter && this.typeFilter != -1){
-      filteredList = filteredList.filter(p=>p.type == this.typeFilter);
+  get filteredProduct() {
+    console.log('test');
+    var filteredList = this.products;
+    if (this.typeFilter && this.typeFilter != -1) {
+      filteredList = filteredList.filter(p => p.type == this.typeFilter);
     }
-    if(this.nameFilter && this.nameFilter != ""){
-      filteredList = filteredList.filter(p=>p.name.toLowerCase().includes(this.nameFilter));
+    if (this.nameFilter && this.nameFilter != '') {
+      filteredList = filteredList.filter(p => p.name.toLowerCase().includes(this.nameFilter));
     }
 
     console.log(filteredList);
     return filteredList;
   }
 
-  tableConsult(data:Product){
-    console.log("consult : " + data.id);
-  }
-  tableEdit(data:Product){
-    console.log("edit : " + data.id);
-  }
-  tableDelete(data:Product){
-    console.log("delete : " + data.id);
+  tableConsult(data: Product) {
+    console.log('consult : ' + data.id);
   }
 
-  newProduct(){
+  tableEdit(data: Product) {
+    console.log('edit : ' + data.id);
+  }
 
-    var product:Product = {
-      id:""+Date.now(),
-      description:"test add",
-      name:"Test Rest Add Generic",
+  tableDelete(data: Product) {
+    console.log('delete : ' + data.id);
+  }
+
+  newProduct() {
+
+    var product: Product = {
+      id: '' + Date.now(),
+      description: 'test add',
+      name: 'Test Rest Add Generic',
       quantity: 20,
       unitOfMeasure: UnitOfMeasure.UNIT,
       unitPrice: 35,
       vat: 0.21,
       type: ProductType.ITEM,
-      uploads:null
-    }
-    console.log(product)
-    this.productRestService.post(product,(data)=>{
-      this.reload();
-    },
-    ()=>{
-      alert("error")
-    },
-    ()=>{
-      alert("product successfully added")
-    })
+      uploads: null
+    };
+    console.log(product);
+    this.productRestService.post(product, (data) => {
+        this.reload();
+      },
+      () => {
+        alert('error');
+      },
+      () => {
+        alert('product successfully added');
+      });
 
   }
-  reload(){
+
+  reload() {
     this.productRestService.get({},
-      (data)=>{ 
+      (data) => {
         console.log(data);
         this.products = data;
-    });
+      });
   }
-  refreshGrid(data){
+
+  refreshGrid(data) {
     this.products = data;
   }
 
