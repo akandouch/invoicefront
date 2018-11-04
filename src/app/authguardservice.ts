852cd4ba70154fbd5c77a5f381faa0b2887d42b4
Nordine Bittich <contact@bittich.be>
2018-11-04 22:04:42 +0100

    role & menu hidden

--- src/app/authguardservice.ts
+++ src/app/authguardservice.ts
@@ -14,7 +14,7 @@ export class AuthGuardService implements CanActivate {
     }
     this.router.navigate(['/error'],
       {
-        queryParams: {label: 'error.forbidden', status: 401, message: 'error.forbidden', returnUrl: state.url}
+        queryParams: {label: 'error.label', status: 401, message: 'error.forbidden', returnUrl: state.url}
       });
     return false;
   }
